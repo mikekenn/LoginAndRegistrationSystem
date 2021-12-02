4ef5b5518c2df3132057b3d4092a836a3099438a
Michael Kennedy <michaelkennedy_228@hotmail.com>
2021-12-01 22:32:54 -0400

    Initial working commit.

--- main.cpp
+++ main.cpp
@@ -29,4 +29,44 @@ bool IsLoggedIn(){
 
 int main(){
 
+    int choice;
+
+    cout << "1: Register\n2: Login\n:Your choice: ";
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        string username, password;
+
+        cout << "select a username: ";
+        cin >> username;
+
+        cout << "select a password: ";
+        cin >> password;
+
+        ofstream file;
+        file.open("data/" + username + ".txt");
+
+        file << username << endl << password;
+
+        file.close();
+    }
+    else if (choice == 2)
+    {
+        bool status = IsLoggedIn();
+
+        if (!status)
+        {
+            cout << "False login!" << endl;
+            system("PAUSE");
+            return 0;
+        }   
+    }
+    else
+    {
+        cout << "Success logged in!" << endl;
+        system("PAUSE");
+        return 1;
+    }
+    
 }
